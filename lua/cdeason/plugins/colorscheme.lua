e184d30b751b3e1a82dc82a64a36f7ecacf5044d
Colby Deason <colbydeason@gmail.com>
2023-11-08 16:07:31 -0600

    Transparent window and treesitter

--- lua/cdeason/plugins/colorscheme.lua
+++ lua/cdeason/plugins/colorscheme.lua
@@ -3,6 +3,8 @@ return{
 	priority = 1000,
 	config = function()
 		vim.cmd([[colorscheme kanagawa-wave]])
+		vim.api.nvim_set_hl(0, "Normal", { bg = "None"})
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None"})
 	end,
 }
 
